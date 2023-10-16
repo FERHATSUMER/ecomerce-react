f9ce8204bb4e0c3631da1286fcca5e1a7194a3e9
FERHATSUMER <sumerf39@gmail.com>
2023-10-16 23:59:19 +0300

    ecomerce-react

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import { FiSettings } from "react-icons/fi";
+import { AiOutlineLogin } from "react-icons/ai";
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  )
-}
+    <div className="header-container">
+      <div className="header-right">
+        <a>
+          <img
+            className="header-logo"
+            src=""
+            alt="logo"
+          />
+        </a>
+        <h1 className="header-title">Ferhat Sümer</h1>
+      </div>
+      <nav>
+        <div className="header-nav-right">
+          <FiSettings />
+          <a href="" target="_blank">
+            <h4></h4>
+          </a>
+          <AiOutlineLogin />
 
-export default Header
+          <a href="" target="_blank">
+            <h4></h4>
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
